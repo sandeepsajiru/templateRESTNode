816bdb0c1b78e935d0144cc792a33f3294edce53
Sandeep <sandeep@beingzero.in>
2016-04-29 01:26:33 -0700

    Brought In Model

---
+++ models/itemModel.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var itemSchema = new mongoose.Schema({
+    title: String,
+    author : String,
+    created_at: Date,
+    updated_at: Date,
+    deleted : Boolean
+});
+
+itemSchema.pre('save', function(next) {
+    // get the current date
+    var currentDate = new Date();
+
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at) {
+        this.created_at = currentDate;
+        this.deleted = false;
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('item', itemSchema);
